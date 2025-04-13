4f176ce8674f2ae6dd42e624e696b6c60b5baf1e
Yerdeee <154613415+Yerdeee@users.noreply.github.com>
2025-04-13 10:00:55 +0000

    Add .csproj

---
+++ week6/csharp/adapter_pattern/AdapterPatternApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// 외부 클래스
+class ExternalClass {
+    public string Search() {
+        return "get user info";
+    }
+}
+
+// 내부 클래스 (인터페이스)
+interface InternalClass {
+    string Fetch();
+}
+
+// 어댑터 클래스
+class Adapter : InternalClass {
+    private ExternalClass external;
+
+    public Adapter(ExternalClass external) {
+        this.external = external;
+    }
+
+    public string Fetch() {
+        return external.Search();
+    }
+}
+
+class Program {
+    static void Main() {
+        ExternalClass external = new ExternalClass();
+        InternalClass adapter = new Adapter(external);
+
+        Console.WriteLine(adapter.Fetch()); // "get user info"
+    }
+}
---
+++ week6/csharp/decorator_pattern/DecoratorPatternApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// 기본 커피 클래스
+class Coffee {
+    public virtual int Cost() {
+        return 5;
+    }
+}
+
+// 커피 데코레이터 클래스 (기본 커피 객체를 감싸는 클래스)
+class CoffeeDecorator : Coffee {
+    protected Coffee coffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    public override int Cost() {
+        return coffee.Cost();
+    }
+}
+
+// 우유 데코레이터 클래스
+class MilkDecorator : CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) : base(coffee) { }
+
+    public override int Cost() {
+        return coffee.Cost() + 2;  // 우유는 2가 추가됨
+    }
+}
+
+// 설탕 데코레이터 클래스
+class SugarDecorator : CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) : base(coffee) { }
+
+    public override int Cost() {
+        return coffee.Cost() + 1;  // 설탕은 1이 추가됨
+    }
+}
+
+class Program {
+    static void Main() {
+        Coffee coffee = new Coffee();
+        Console.WriteLine(coffee.Cost());  // 5
+
+        Coffee coffeeWithMilk = new MilkDecorator(coffee);
+        Console.WriteLine(coffeeWithMilk.Cost());  // 7
+
+        Coffee coffeeWithSugar = new SugarDecorator(coffee);
+        Console.WriteLine(coffeeWithSugar.Cost());  // 6
+
+        Coffee coffeeWithMilkAndSugar = new SugarDecorator(coffeeWithMilk);
+        Console.WriteLine(coffeeWithMilkAndSugar.Cost());  // 8
+    }
+}
